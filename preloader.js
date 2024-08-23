7346887f0ce10ea76e8877d7afb4a091768f8df0
Virtualizei <contato@virtualizei.com.br>
2024-08-23 08:17:12 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -7,8 +7,8 @@ overlay.style.cssText = `
     position: fixed;
     top: 0;
     left: 0;
-    width: 100%;
-    height: 100%;
+    width: 30%;
+    height: 30%;
     background-color: rgba(255, 255, 255, 0.7);
     z-index: 999;
 `;
